7120cb092ff93e6ed8f34fb4dc165291f7dc872e
DZhang3210 <53838139+DZhang3210@users.noreply.github.com>
2024-07-04 18:33:42 -0400

    changing suspense again

--- utils/database.js
+++ utils/database.js
@@ -13,8 +13,6 @@ export const connectToDb = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
       dbName: "share_prompt",
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     })
 
     isConnected = true;
